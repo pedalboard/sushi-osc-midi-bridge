9cdd46a8e1c68ce8009e7297ddd15fadd6a3e979
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-07-15 16:52:31 +0200

    Get control change

--- cmd/sushi_midi_osc_bridge.go
+++ cmd/sushi_midi_osc_bridge.go
@@ -18,15 +18,10 @@ func main() {
 	}
 
 	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
-		var bt []byte
-		var ch, key, vel uint8
+		var ch, ctrl, vel uint8
 		switch {
-		case msg.GetSysEx(&bt):
-			fmt.Printf("got sysex: % X\n", bt)
-		case msg.GetNoteStart(&ch, &key, &vel):
-			fmt.Printf("starting note %s on channel %v with velocity %v\n", midi.Note(key), ch, vel)
-		case msg.GetNoteEnd(&ch, &key):
-			fmt.Printf("ending note %s on channel %v\n", midi.Note(key), ch)
+		case msg.GetControlChange(&ch, &ctrl, &vel):
+			fmt.Printf("starting control change %v on channel %v with value %v\n", ctrl, ch, vel)
 		default:
 			// ignore
 		}
