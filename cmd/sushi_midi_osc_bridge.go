550f91ced0e43e56d54f4b94ac69e73048ad1eaf
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-07-15 18:38:36 +0200

    Connect to sushi via GRPC

--- cmd/sushi_midi_osc_bridge.go
+++ cmd/sushi_midi_osc_bridge.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/pedalboard/somb/internal/sushi_rpc"
 	"gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv" // autoregisters driver
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+const midi_port = "pedalboard-midi"
+
 func main() {
+	ctx := context.Background()
+	conn, err := grpc.Dial("pedalboard-dev:51051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to dail sushi grpc port: %v", err)
+	}
+	defer conn.Close()
+	err = connect_to_sushi(ctx, conn)
+	if err != nil {
+		log.Fatalf("failed to connect to sushi: %v", err)
+	}
+
 	defer midi.CloseDriver()
 
-	in, err := midi.FindInPort("pedalboard-midi")
+	in, err := midi.FindInPort(midi_port)
 	if err != nil {
-		fmt.Println("can't find port")
-		return
+		log.Fatalf("failed opening midi port: %v", err)
 	}
 
 	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
@@ -41,4 +58,14 @@ func main() {
 	<-sigchan
 	stop()
 	os.Exit(0)
+}
+
+func connect_to_sushi(ctx context.Context, cc grpc.ClientConnInterface) error {
+	sc := sushi_rpc.NewSystemControllerClient(cc)
+	v, err := sc.GetSushiVersion(ctx, &sushi_rpc.GenericVoidValue{})
+	if err != nil {
+		return fmt.Errorf("failed to get sushi version: %w", err)
+	}
+	log.Printf("connected to sushi version: %v", v.Value)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,14 @@ go 1.20
 
 require (
 	gitlab.com/gomidi/midi/v2 v2.0.30
+	google.golang.org/grpc v1.56.2
 	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 )
