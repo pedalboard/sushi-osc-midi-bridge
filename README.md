a5484aeaccf744f089a49478fda36d42fcd2c21b
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-07-15 20:00:43 +0200

    SetProcessorByPassState

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # sushi-osc-midi-bridge
 
-converts midi messages to OSC messages
+
+Due to some limitations of the midi processing within the ELK Audio Sushi DAW, this
+deamon allows to listen to the midi input and converts some of the messages to OSC messages:
+
+| MIDI          | Condition                   | Sushi Command                              |
+|---------------|-----------------------------|--------------------------------------------|
+| ControlChange | Value < 64 && Control < 10  | Bypass Processor (ID=Control, Value=true)  |
+| ControlChange | Value >= 64 && Control < 10 | Bypass Processor (ID=Control, Value=false) |
--- cmd/sushi_midi_osc_bridge.go
+++ cmd/sushi_midi_osc_bridge.go
@@ -23,7 +23,10 @@ func main() {
 		log.Fatalf("failed to dail sushi grpc port: %v", err)
 	}
 	defer conn.Close()
-	err = connect_to_sushi(ctx, conn)
+
+	sushi := NewSushi(conn)
+
+	err = sushi.CheckConnection(ctx)
 	if err != nil {
 		log.Fatalf("failed to connect to sushi: %v", err)
 	}
@@ -36,10 +39,14 @@ func main() {
 	}
 
 	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
-		var ch, ctrl, vel uint8
+		var ch, ctrl, value uint8
 		switch {
-		case msg.GetControlChange(&ch, &ctrl, &vel):
-			fmt.Printf("starting control change %v on channel %v with value %v\n", ctrl, ch, vel)
+		case msg.GetControlChange(&ch, &ctrl, &value):
+			if ch == 2 && ctrl < 10 {
+				bypassed := value < 64
+				sushi.SetProcessorBypassState(ctx, int32(ch), bypassed)
+			}
+
 		default:
 			// ignore
 		}
@@ -60,12 +67,35 @@ func main() {
 	os.Exit(0)
 }
 
-func connect_to_sushi(ctx context.Context, cc grpc.ClientConnInterface) error {
-	sc := sushi_rpc.NewSystemControllerClient(cc)
+type Sushi struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewSushi(cc grpc.ClientConnInterface) *Sushi {
+	return &Sushi{cc: cc}
+}
+
+func (s *Sushi) CheckConnection(ctx context.Context) error {
+	sc := sushi_rpc.NewSystemControllerClient(s.cc)
 	v, err := sc.GetSushiVersion(ctx, &sushi_rpc.GenericVoidValue{})
 	if err != nil {
 		return fmt.Errorf("failed to get sushi version: %w", err)
 	}
 	log.Printf("connected to sushi version: %v", v.Value)
+	return nil
+}
+
+func (s *Sushi) SetProcessorBypassState(ctx context.Context, id int32, bypassed bool) error {
+
+	agc := sushi_rpc.NewAudioGraphControllerClient(s.cc)
+
+	_, err := agc.SetProcessorBypassState(ctx, &sushi_rpc.ProcessorBypassStateSetRequest{
+		Processor: &sushi_rpc.ProcessorIdentifier{Id: id},
+		Value:     bypassed,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to SetProcessorBypassState: %w", err)
+	}
+	fmt.Printf("SetProcessorBypassState for processor %v to %v", id, bypassed)
 	return nil
 }
